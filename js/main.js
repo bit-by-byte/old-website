4f8c553c6211b37180e5055b51852b2e9ae14ce8
Ayush <61765149+citricacid2@users.noreply.github.com>
2021-08-03 12:39:53 -0400

    Rewrote Site with Bulma css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".typed", {
+    stringsElement: ".typed-phrases",
+    // showCursor: false,
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+});
