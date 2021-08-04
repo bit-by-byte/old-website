f8a61afa88e602f464a7be447b6a0626acd6b52b
Ayush <61765149+citricacid2@users.noreply.github.com>
2021-08-04 10:36:25 -0400

    Finished up bulma port

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ let typed = new Typed(".typed", {
     backSpeed: 100,
     backDelay: 1000,
 });
+
+
+const nav = document.querySelector('.navbar-burger');
+
+nav.addEventListener('click', () => {
+    nav.classList.toggle('is-active')
+    document.getElementById(nav.dataset.target).classList.toggle('is-active')
+})
